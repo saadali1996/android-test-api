66987dfd694f1d04a79d9a2c07e8f49f2d6e5622
Muhammad Saad Ali <33375892+saadali1996@users.noreply.github.com>
2018-03-30 17:27:28 +0500

    test update commit

--- TesterLib/src/androidTest/java/org/hopto/apimatic/controllers/BodyParamsControllerTest.java
+++ TesterLib/src/androidTest/java/org/hopto/apimatic/controllers/BodyParamsControllerTest.java
@@ -1,7 +1,8 @@
 /*
- * TesterLib
- *
- * This file was automatically generated for Stamplay by APIMATIC v2.0 ( https://apimatic.io ).
+ * Muhammad Saad ALi
+ *i140057
+ * Software Testing Assignment2
+
  */
 package org.hopto.apimatic.controllers;
 
